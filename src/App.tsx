986b5b1eccf74690bfca25f2e5fe735aa724a28f
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-21 12:48:09 +0900

    実装（まだ動かない）

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Line } from "react-chartjs-2";
 
 function App() {
+  const labels = ["1 月", "2 月", "3 月", "4 月", "5 月", "6 月"];
+  const graphData = {
+    // eslint-disable-next-line object-shorthand
+    labels: labels,
+    datasets: [
+      {
+        label: "A社",
+        data: [65, 59, 60, 81, 56, 55],
+        borderColor: "rgb(75, 192, 192)",
+      },
+      {
+        label: "B社",
+        data: [60, 55, 57, 61, 75, 50],
+        borderColor: "rgb(75, 100, 192)",
+      },
+    ],
+  };
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const options: {} = {
+    maintainAspectRatio: false,
+  };
+
+  const divStyle: React.CSSProperties = {
+    marginLeft: "auto",
+    marginRight: "auto",
+    margin: "10px",
+    width: "500px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={divStyle}>
+      <Line
+        height={300}
+        width={300}
+        data={graphData}
+        options={options}
+        id="chart-key"
+      />
     </div>
   );
 }
